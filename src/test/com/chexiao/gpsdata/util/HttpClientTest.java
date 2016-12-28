22fe9da5758759c1475ab8ca571be38e8433f812
fulei <fulei@daojia.com>
2016-12-28 17:28:58 +0800

    火星转换

--- src/test/com/chexiao/gpsdata/util/HttpClientTest.java
+++ src/test/com/chexiao/gpsdata/util/HttpClientTest.java
@@ -9,7 +9,10 @@ public class HttpClientTest {
 
     @Test
     public void getBaidulantiAndLongti() {
-        String result = BaiduUtil.testGetBaiduLnti(Double.valueOf("30.4967"),Double.valueOf("104.0768"));
+        //106.090483,25.1722
+        //104.054283,30.5894
+       // 30.493734247746247,104.08021888698887
+        String result = BaiduUtil.testGetBaiduLnti(Double.valueOf("30.493734247746247"),Double.valueOf("104.08021888698887"));
         System.out.println(result);
     }
 }
