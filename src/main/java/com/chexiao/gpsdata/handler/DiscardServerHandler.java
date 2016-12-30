d3a37b3e914e71f3f782fc177e40b17f56a34a5e
fulei <fulei@daojia.com>
2016-12-30 14:47:53 +0800

    分库分表,deviceid解析

--- src/main/java/com/chexiao/gpsdata/handler/DiscardServerHandler.java
+++ src/main/java/com/chexiao/gpsdata/handler/DiscardServerHandler.java
@@ -1,6 +1,5 @@
 package com.chexiao.gpsdata.handler;
 
-import com.chexiao.gpsdata.entity.Student;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
